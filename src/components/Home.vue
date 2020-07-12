00cc2b4e07f7128c9d3b814ea8274ce7c6356a75
Jonathan Cuesto <34cuesto@gmail.com>
2020-07-11 21:57:17 -0400

    Home page added

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-layout align-start>
+    <v-flex>
+      <v-card>
+        <v-container>
+          <h1>In Development</h1>
+        </v-container>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  components: {},
+  data: () => ({}),
+  mounted() {},
+
+  methods: {}
+};
+</script>
