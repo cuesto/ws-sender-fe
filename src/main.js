130aee2dd6dc49352aef217aecaacf28a62ee495
Jonathan Cuesto <34cuesto@gmail.com>
2020-08-08 00:20:02 -0400

    .

--- src/main.js
+++ src/main.js
@@ -6,9 +6,6 @@ import vuetify from './plugins/vuetify';
 import VueSweetalert2 from "vue-sweetalert2";
 // // If you don't need the styles, do not connect
 // import 'sweetalert2/dist/sweetalert2.min.css';
-import Swal from 'sweetalert2/dist/sweetalert2.js'
-
-import 'sweetalert2/src/sweetalert2.scss'
 
 Vue.config.productionTip = false;
 
