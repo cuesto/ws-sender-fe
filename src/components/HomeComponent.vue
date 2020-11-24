9faceead848ff89353d60556f854907004bb6a68
Jonathan Cuesto <34cuesto@gmail.com>
2020-11-23 20:23:46 -0400

    buttong upload added

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -7,12 +7,22 @@
         sort-by="location"
         class="elevation-1"
         :items-per-page="10"
-        :footer-props="{'items-per-page-options': [10, 15, 20, 25]  }"
+        :footer-props="{ 'items-per-page-options': [10, 15, 20, 25] }"
       >
         <template v-slot:top>
           <v-toolbar flat color="white">
             <v-toolbar-title>Conteo de Inventario</v-toolbar-title>
             <v-divider class="mx-4" inset vertical></v-divider>
+            <v-btn
+              :loading="loading"
+              :disabled="loading"
+              color="green"
+              class="ma-2 white--text"
+              @click="loader = 'loading'"
+            >
+              Cargar
+              <v-icon right dark> mdi-cloud-upload </v-icon>
+            </v-btn>
             <v-spacer></v-spacer>
             <v-text-field
               class="text-xs-center"
@@ -26,12 +36,14 @@
             <v-spacer></v-spacer>
             <v-chip class="ma-2" color="green" outlined>
               Ubicación :
-              <b>{{location}}</b>
+              <b>{{ location }}</b>
             </v-chip>
           </v-toolbar>
         </template>
         <template v-slot:item.options="{ item }">
-          <v-icon size="sm" color="red" class="mr-1" @click="deleteItem(item)">mdi-delete</v-icon>
+          <v-icon size="sm" color="red" class="mr-1" @click="deleteItem(item)"
+            >mdi-delete</v-icon
+          >
         </template>
       </v-data-table>
     </v-flex>
@@ -50,7 +62,19 @@ export default {
     items: [],
     sku: "",
     location: "",
+    loader: null,
+    loading: false,
   }),
+  watch: {
+    loader() {
+      const l = this.loader;
+      this[l] = !this[l];
+
+      setTimeout(() => (this[l] = false), 3000);
+
+      this.loader = null;
+    },
+  },
   mounted() {},
 
   methods: {
