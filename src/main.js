b5ee2ee3778fc2f8859bda679e377e6f58346aa5
Jonathan Cuesto <34cuesto@gmail.com>
2020-12-06 00:24:43 -0400

    firebase cloud storage configured

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import { auth } from './firebase';
 //import VueSweetalert2 from "vue-sweetalert2";
 // // If you don't need the styles, do not connect
 // import 'sweetalert2/dist/sweetalert2.min.css';
@@ -11,9 +12,21 @@ Vue.config.productionTip = false;
 
 //Vue.use(VueSweetalert2);
 
-new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app');
+// new Vue({
+//   router,
+//   store,
+//   vuetify,
+//   render: h => h(App)
+// }).$mount('#app');
+
+
+let app
+auth.onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
