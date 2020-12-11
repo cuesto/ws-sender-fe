ced51b8ef5170a84b7c4e9057cb7f6e759af5f61
Jonathan Cuesto <34cuesto@gmail.com>
2020-12-10 21:34:28 -0400

    form login added and creation of users in firebase

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 import { auth } from './firebase';
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 //import VueSweetalert2 from "vue-sweetalert2";
 // // If you don't need the styles, do not connect
 // import 'sweetalert2/dist/sweetalert2.min.css';
@@ -24,6 +25,7 @@ let app
 auth.onAuthStateChanged(() => {
   if (!app) {
     app = new Vue({
+      vuetify,
       router,
       store,
       render: h => h(App)
