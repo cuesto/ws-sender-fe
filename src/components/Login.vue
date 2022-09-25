d6c66c8ce9bac7a6ae966ef1a7ceac0e9044c380
Jonathan Cuesto <34cuesto@gmail.com>
2022-09-25 10:40:00 -0400

    v-text-field pattern

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -126,7 +126,7 @@ export default {
     rules: {
       required: (value) => !!value || "Requerido.",
       email: (value) => {
-        const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return pattern.test(value) || "Correo Inválido.";
       },
     },
